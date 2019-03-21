d415928141bd8c73597d3afd802fc1d4e1c3c0c0
Grant MacDonald <phisoluciones.es@gmail.com>
2019-03-21 16:05:18 +0100

    change to mail()

--- app/Classes/PermissionLogic.php
+++ app/Classes/PermissionLogic.php
@@ -22,6 +22,15 @@ class PermissionLogic {
         $username = ( $username !== null ) ? $username : $_REQUEST['username'];
 
         $admin = self::ADMINISTRATORS;
-        \Mail::to( $admin )->send( new AdminCheckUsername( $username ) );
+        $headers = "Reply-To: ".env('FROM_EMAIL')."\r\n"; 
+        $headers .= "Return-Path: ".env('FROM_EMAIL')."\r\n";
+        $headers .= "From: ".env('FROM_EMAIL')."\r\n"; 
+        $headers .= "Organization: Couriers Connect\r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+        $headers .= "X-Priority: 3\r\n";
+        $headers .= "X-Mailer: PHP". phpversion() ."\r\n"; 
+        \mail($admin, "New user", view('admin/usernamemail', array( "username" => $username ) ), $headers );
+        // \Mail::to( $admin )->send( new AdminCheckUsername( $username ) );
     }
 }
