dbb50a42c4dbe9fc748004cf5c11d6869597155b
Grant MacDonald <phisoluciones.es@gmail.com>
2019-05-09 22:37:51 +0200

    streamline loaded files

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -19,23 +19,16 @@
         <link rel="icon" type="image/vnd.microsoft.icon" href="couriers-service-quote/courier-quote-compare.ico" sizes="16x16">
         <link rel="stylesheet" href="{{ asset('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' ) }}" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="{{ asset('/css/main.css?version=1.2')}}">
-        <link type="text/css" rel="stylesheet" href="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid.min.css')}}" />
-        <link type="text/css" rel="stylesheet" href="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid-theme.min.css')}}" />
         <link rel="stylesheet" href="{{ asset('/css/jquery-ui.min.css')}}"></link>
         <link rel="stylesheet" href="{{ asset('https://use.fontawesome.com/releases/v5.3.1/css/all.css')}}" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-        <link rel="stylesheet" href="{{ asset('/css/tooltipster.bundle.min.css') }}">
         <link rel="stylesheet" href="{{ asset('/css/slick.css') }}">
         <link rel="stylesheet" href="{{ asset('/css/slick-theme.css') }}">
     </head>
     <body>
         <script src="{{ asset('https://code.jquery.com/jquery-3.3.1.min.js')}}" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="{{ asset('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js')}}" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid.min.js')}}"></script>
-        <script src="{{ asset('/js/jsrender.min.js')}}"></script>
         <script src="{{ asset('/js/jquery-ui.min.js')}}"></script>
-        <script src="{{ asset('/js/notify.min.js')}}"></script>
         <script src="{{ asset('/js/slick.min.js')}}"></script>
-        <script src="{{ asset('/js/tooltipster.bundle.min.js') }}"></script>
         <script src="{{ asset('/js/main.js')}}"></script>
         {!! $set_menu !!}
         {!! $header !!}
