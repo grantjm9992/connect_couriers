a009d8873be2fd2e07d6fb71d3e4c62ad0c93bb0
Grant MacDonald <phisoluciones.es@gmail.com>
2019-06-10 19:46:31 +0200

    CC0003
    Add link to user profile

--- resources/views/deliveries/detail.blade.php
+++ resources/views/deliveries/detail.blade.php
@@ -38,6 +38,11 @@
             <div class="col-12 col-md-8" style="padding: 0 0 0 10px;">
                 <div class="container-fluid" style="border-left: 1px solid rgba(0, 0, 0, 0.15);">
                     <p>
+                        <a href="Users?id={{$data->id_user}}">
+                            {{ $data->str_user }} ({{ $data->feedback_user }})
+                        </a>
+                    </p>
+                    <p>
                         {{ $data->str_title }}
                     </p>
                     <p>
