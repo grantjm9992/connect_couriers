8c027fa8e44cdfda37e0c9761853e240fc619656
Grant MacDonald <phisoluciones.es@gmail.com>
2019-06-10 19:20:50 +0200

    CC0010

--- public/js/main.js
+++ public/js/main.js
@@ -11,6 +11,43 @@ $(document).on('click', 'a[href^="/#"]', function (event) {
         }, 1000);
     }
 });
+$(document).ready( function() {
+	$('img').click( function() {
+		closeHolder();
+		var url = $(this).attr('src');
+		var holder = document.createElement('div');
+		holder.id = "img_holder";
+		$(holder).css({
+			height: "100vh",
+			width: "100vw",
+			background: "rgba(0,0,0,0.75)",
+			position: "fixed",
+			top: 0,
+			left: 0,
+			padding: "50px",
+			lineHeight: "calc(100vh - 100px)",
+			textAlign: "center"
+		});
+		$(holder).html('<img src="'+url+'" style="max-height: 100%; max-width: 100%;">');
+		$('body').append(holder);
+		$(holder).click( function() {
+			closeHolder();
+		});
+	});
+	document.addEventListener("keydown", function(event) {
+		if ( event.which === 27 )
+		{
+			closeHolder();
+		}
+	});
+
+});
+
+function closeHolder()
+{
+	$('#img_holder').remove();
+}
+
 
 function listingDetail(row) {
     window.location = "Deliveries.detail?id_listing="+row.data.id;
